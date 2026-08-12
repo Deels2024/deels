b655a91795b85d3b4baf765b11c315f4d9e249e1
Deels2024 <160591824+Deels2024@users.noreply.github.com>
2026-08-12 14:08:12 +0300

    Load functional hub styles after source design

--- resources/views/layouts/neon/header.blade.php
+++ resources/views/layouts/neon/header.blade.php
@@ -32,6 +32,7 @@
     <link rel="stylesheet" href="{{ext_asset('/dist/css/deelsweb-campaign-detail-source.css')}}"/>
     <link rel="stylesheet" href="{{ext_asset('/dist/css/deelsweb-dashboard-source.css')}}"/>
     <link rel="stylesheet" href="{{ext_asset('/dist/css/deelsweb-create-source.css')}}"/>
+    <link rel="stylesheet" href="{{ext_asset('/dist/css/deelsweb-functional-source.css')}}"/>
     <script defer src="{{ext_asset('/dist/js/deels-v2.js')}}"></script>
     @yield('page-css')
     <script>
