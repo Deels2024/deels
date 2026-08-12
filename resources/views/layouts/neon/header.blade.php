deaf4bb91e6db2f643e4d7c05d5543fd102153ef
Deels2024 <160591824+Deels2024@users.noreply.github.com>
2026-08-12 17:57:06 +0300

    Load complete new footer design layer

--- resources/views/layouts/neon/header.blade.php
+++ resources/views/layouts/neon/header.blade.php
@@ -40,10 +40,12 @@
     <link rel="stylesheet" href="{{ext_asset('/dist/css/deelsweb-messages-functions-source.css')}}"/>
     <link rel="stylesheet" href="{{ext_asset('/dist/css/deelsweb-admin-source.css')}}"/>
     <link rel="stylesheet" href="{{ext_asset('/dist/css/deelsweb-admin-lists-source.css')}}"/>
+    <link rel="stylesheet" href="{{ext_asset('/dist/css/deelsweb-footer-source.css')}}"/>
     <link rel="stylesheet" href="{{ext_asset('/dist/css/deelsweb-qa-source.css')}}"/>
     <script defer src="{{ext_asset('/dist/js/deels-v2.js')}}"></script>
     <script defer src="{{ext_asset('/dist/js/deels-functional-v2.js')}}"></script>
     <script defer src="{{ext_asset('/dist/js/deels-admin-v2.js')}}"></script>
+    <script defer src="{{ext_asset('/dist/js/deels-footer-v2.js')}}"></script>
     @yield('page-css')
     <script>
         window.Laravel = {!! json_encode(['csrfToken' => csrf_token()]) !!};
