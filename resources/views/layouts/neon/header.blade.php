a04b22c494de7d8fbb79a70c2ffcbb614ceac116
Deels2024 <160591824+Deels2024@users.noreply.github.com>
2026-08-12 16:29:17 +0300

    Load moderation list styling in dashboard layout

--- resources/views/layouts/neon/header.blade.php
+++ resources/views/layouts/neon/header.blade.php
@@ -38,6 +38,7 @@
     <link rel="stylesheet" href="{{ext_asset('/dist/css/deelsweb-profile-functions-source.css')}}"/>
     <link rel="stylesheet" href="{{ext_asset('/dist/css/deelsweb-messages-functions-source.css')}}"/>
     <link rel="stylesheet" href="{{ext_asset('/dist/css/deelsweb-admin-source.css')}}"/>
+    <link rel="stylesheet" href="{{ext_asset('/dist/css/deelsweb-admin-lists-source.css')}}"/>
     <script defer src="{{ext_asset('/dist/js/deels-v2.js')}}"></script>
     <script defer src="{{ext_asset('/dist/js/deels-functional-v2.js')}}"></script>
     <script defer src="{{ext_asset('/dist/js/deels-admin-v2.js')}}"></script>
